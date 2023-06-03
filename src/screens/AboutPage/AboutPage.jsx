386629fddc78a06065a8777202483821fbde71cc
Artem Bobylev <artematrr@gmail.com>
2023-06-03 17:24:40 +0400

    add about content

--- src/screens/AboutPage/AboutPage.jsx
+++ src/screens/AboutPage/AboutPage.jsx
@@ -4,10 +4,13 @@ const AboutPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <h1>About</h1>
-        <p>
-          This is a simple app to help you keep track of your expenses. It is a
-          work in progress.
+        <h1>Обо мне</h1>
+        <p>Бобылев Артем Андреевич</p>
+        <p>React Developer</p>
+        <p className={styles.aboutLink}>
+          <a href='https://vk.com/artematrr'>VK</a> <span> | </span>
+          <a href='https://github.com/artematrr'>GH</a> <span> | </span>
+          <a href=' https://t.me/artematrr'>TG</a>
         </p>
       </div>
     </div>
