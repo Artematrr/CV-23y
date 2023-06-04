4600cf6ccae763621722ca8042bb17658af0c50c
Artem Bobylev <artematrr@gmail.com>
2023-06-04 14:02:07 +0400

    add Breadcrumbs

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './elements/Footer/Footer'
 import Header from './elements/Header/Header'
+import Breadcrumbs from './elements/Breadcrumbs/Breadcrumbs'
 
 function Layout() {
   return (
     <>
       <Header />
+      <Breadcrumbs />
       <Outlet />
       <Footer />
     </>
